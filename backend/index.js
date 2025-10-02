81b16e23dd5978b4bc65c4d03def131e79dfb6b1
naiello <nadialucianaaiello@gmail.com>
2025-10-02 01:57:13 -0400

    servidor con express y rutas simples

---
+++ backend/index.js
@@ -0,0 +1,70 @@
+// 1. Requerir modulos
+import express from 'express';
+import { readFile } from 'fs/promises'; // Para la versión asíncrona
+
+// 2. Crear una instancia de la aplicación
+const app = express();
+ 
+// 3. Definir el puerto. Es una buena práctica usar una variable de entorno para producción.
+const PORT = 4000;
+ 
+// 4. Definir nuestra primera ruta (endpoint)
+// Cuando un cliente haga una petición GET a la raíz ('/'), se ejecutará esta función.
+app.get('/', (req, res) => {
+  res.send('¡Bienvenido al API de Mueblería Jota!');
+});
+
+app.get('/', (req, res) => {
+  res.send('¡Bienvenido al API de Mueblería Jota!');
+});
+ 
+// GET para obtener todos los productos
+app.get('/api/productos', async (req, res) => {
+  try {
+    const datos = await cargarDatos();
+    res.json(datos);  // mejor usar .json() para enviar objetos
+  } catch (error) {
+    res.status(500).send('Error al obtener los datos');
+  }
+});
+
+app.get('/api/productos/:id', async (req, res) => {
+  try {
+    const datos = await cargarDatos();
+    
+    const id = parseInt(req.params.id); // el :id viene como string, lo pasamos a número
+    console.log(id);
+    
+    const producto = datos.find(p => p.id === id); // buscamos por id
+    console.log(id,producto);
+    
+
+    if (producto) {
+      res.json(producto);
+    } else {
+        console.log(id,producto)
+      res.status(404).json({ mensaje: "Producto no encontrado" });
+    }
+  } catch (error) {
+    res.status(500).send("Error al obtener el producto");
+  }
+});
+
+ 
+// 5. Poner el servidor a escuchar peticiones
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo exitosamente en http://localhost:${PORT}`);
+});
+
+
+async function cargarDatos() {
+  try {
+    const data = await readFile('muebles.json', { encoding: 'utf8' });
+    const datos = JSON.parse(data);
+    return datos;  
+  } catch (error) {
+    console.error('Error al leer o parsear el JSON:', error);
+    throw error;
+  }
+}
+
