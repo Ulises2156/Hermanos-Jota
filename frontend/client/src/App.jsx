64cbfbe3244c60ceaeb21e68219f146b00ddfe1d
Ulises2156 <ulisespavon008@gmail.com>
2025-10-06 19:22:09 -0300

    App desplegada

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import Layout from './components/Layout';
 import Home from './pages/Home';
--- frontend/client/src/main.jsx
+++ frontend/client/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/globals.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter basename='/Hermanos-Jota'>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
