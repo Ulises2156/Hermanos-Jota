f9a283ea989cae280f48c50cc1cc30b8d7bc52a8
Ulises2156 <ulisespavon008@gmail.com>
2025-10-06 20:22:04 -0300

    limpiando el  gh-pages

--- frontend/client/src/api/products.js
+++
@@ -1,9 +0,0 @@
-export async function fetchProducts() {
-  try {
-    const res = await fetch('/api/productos')
-    if (!res.ok) throw new Error(`HTTP ${res.status}`)
-    return await res.json()
-  } catch (err) {
-    throw new Error('No se pudo cargar el catálogo')
-  }
-}
