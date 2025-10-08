b51edeb9f5c1397e1e252500096240192064e5a4
Ulises2156 <ulisespavon008@gmail.com>
2025-10-07 22:26:33 -0300

    Modificando el backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 // 1. Requerir modulos
 import express from 'express';
+import cors from 'cors'; // para permitir CORS
 import { readFile } from 'fs/promises'; // Para la versión asíncrona
+import productoRouter from './routes/productos.js';
 
 // 2. Crear una instancia de la aplicación
 const app = express();
@@ -8,18 +10,54 @@ const app = express();
 // 3. Definir el puerto. Es una buena práctica usar una variable de entorno para producción.
 const PORT = 3000;
  
+
+//Cors
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://ulises2156.github.io'], // Reemplaza con el origen de tu frontend
+  credentials: true
+}));
+
+// Middleware global: para los log de cada request
+
+app.use((req, res, next) =>{
+  console.log(`[${new Date().toDateString()}] ${req.method} ${req.url}`);
+  next();
+});
+
+
+ 
 // 4. Definir nuestra primera ruta (endpoint)
 // Cuando un cliente haga una petición GET a la raíz ('/'), se ejecutará esta función.
 app.get('/', (req, res) => {
   res.send('¡Bienvenido al API de Mueblería Jota!');
 });
 
-app.get('/', (req, res) => {
-  res.send('¡Bienvenido al API de Mueblería Jota!');
+// usarmos el router de productos
+app.use('/api/productos', productoRouter);  
+
+//Middleware 404 (si no se encuentra la ruta)
+app.use((req, res)=>{
+ res.status(404).json({mensaje: "Ruta no encontrada"}); 
 });
- 
+
+
+//Middleware para centralizar errores
+app.use((err, req, res, next) =>{
+  console.log('Error interno:', err);
+  res.status(500).json({mensaje: 'Error interno del servidor'});
+});
+
+
+
+// 5. Poner el servidor a escuchar peticiones
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo exitosamente en http://localhost:${PORT}`);
+});
+
+
+
 // GET para obtener todos los productos
-app.get('/api/productos', async (req, res) => {
+/*app.get('/api/productos', async (req, res) => {
   try {
     const datos = await cargarDatos();
     res.json(datos);  // mejor usar .json() para enviar objetos
@@ -48,36 +86,14 @@ app.get('/api/productos/:id', async (req, res) => {
   } catch (error) {
     res.status(500).send("Error al obtener el producto");
   }
-});
+});*/
 
  
-// 5. Poner el servidor a escuchar peticiones
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo exitosamente en http://localhost:${PORT}`);
-});
 
-// Middleware global: para los log de cada request
-
-app.use((req, res, next) =>{
-  console.log(`[${new Date().toDateString()}] ${req.method} ${req.url}`);
-  next();
-});
-
-// Importo las rutas de productos
+/*// Importo las rutas de productos
 import productoRouter from './routes/productos.js';
 app.use('/api/productos', productoRouter);
 
-
-//Middleware 404 (si no se encuentra la ruta)
-app.use((req, res)=>{
- res.status(404).json({mensaje: "Ruta no encontrada"}); 
-});
-
-//Middleware para centralizar errores
-app.use((err, req, res, next) =>{
-  console.log('Error interno:', err);
-  res.status(500).json({mensaje: 'Error interno del servidor'});
-});
 
 
 
@@ -91,4 +107,4 @@ async function cargarDatos() {
     throw error;
   }
 }
-
+*/
